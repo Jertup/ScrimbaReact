28662710533abf8c68f83a4c4040460b4f425bc7
Jertup <jere.tupp@gmail.com>
2025-06-04 14:48:16 +0300

    Adding the data js file for the jokes

---
+++ src/Jokes/jokesData.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        setup: "I got my daughter a fridge for her birthday.",
+        punchline: "I can't wait to see her face light up when she opens it.",
+        upvotes: 35,
+        isPun: true,
+        comments: ["Hilarious!", "Best dad joke ever.", "Love this one!"]
+    },
+    {
+        setup: "How did the hacker escape the police?",
+        punchline: "He just ransomware!",
+        upvotes: 56,
+        isPun: true,
+        comments:["Funny!", "Clever!", "I see what you did there."]
+    },
+    {
+        setup: "Why don't pirates travel on mountain roads?",
+        punchline: "Scurvy.",
+        upvotes: 936,
+        isPun: true,
+        comments:[
+          {text: "Arr, that's a good one!", author: "Captain Jack"},
+          {text: "I sea what you did there!", author: "First Mate"},
+          {text: "Aye, matey!", author: "Sailor Sam"}
+        ]
+    },
+    {
+        setup: "Why do bees stay in the hive in the winter?",
+        punchline: "Swarm."
+    },
+    {
+        setup: "What's the best thing about Switzerland?",
+        punchline: "I don't know, but the flag is a big plus!"
+    }
+]
