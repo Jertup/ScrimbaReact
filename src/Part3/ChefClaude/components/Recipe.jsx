9a6b5a1ddb0119783fa8fef892570f15186afa98
Jertup <jere.tupp@gmail.com>
2025-06-13 10:45:32 +0300

    Add AI to ChefClaude and add SoundPads folder

--- src/Part3/ChefClaude/components/Recipe.jsx
+++ src/Part3/ChefClaude/components/Recipe.jsx
@@ -1,17 +1,35 @@
-import react from "react";
-
-function Recipe() {
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+function Recipe(props) {
+    const { ingredientList, fetchRecipe, loading, recipeText } = props;
 
     return (
+      <section className="ingredient-section">
+        <h1 className="ingredient-header">Ingredients on hand:</h1>
+        <ul className="ingredient-list">
+          {ingredientList}
+        </ul>
+        {ingredientList.length >= 4 && 
         <div className="recipe-button-container">
-            <div className="recipe-header">
-                <h2>Ready for a recipe?</h2>
-                <p>Generate a recipe from your list of ingredients.</p>
-            </div>
-            <button 
-                className="generate-recipe-button"
-            >Get a recipe</button>
+          <div className="recipe-header">
+            <h2>Ready for a recipe?</h2>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button
+            className="generate-recipe-button"
+            onClick={fetchRecipe}
+            disabled={loading}
+          >
+            {loading ? "Generating..." : "Get a recipe"}
+          </button>
         </div>
+        }
+      {recipeText && (
+        <div className="recipe-markdown">
+          <ReactMarkdown>{recipeText}</ReactMarkdown>
+        </div>
+      )}
+      </section>
     )
 
 
